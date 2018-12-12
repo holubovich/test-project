710efa18980d1fe8e1897e47bce8fbf50bcb7598
Elena Golubovich <elenaholubovich988@gmail.com>
2018-12-13 00:25:08 +0300

    Add Button

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react'
 import classes from './Quiz.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component{
 
     state={
+        results:{},//{[id]: 'success' 'error'}
+        isFinished:false,
         activeQuestion:0,
         answerState: null, //{[id]: 'success' 'error'}
         quiz:[{
@@ -37,17 +40,30 @@ class Quiz extends Component{
     }
 
     onAnswerClickHandler = answerId => {
+        if(this.state.answerState){
+            const key = Object.keys(this.state.answerState)[0]
+            if(this.state.answerState[key] === 'success'){
+             return
+            }
+        }
 
-        const question= this.state.quiz[this.state.activeQuestion]
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
 
         if(question.rightAnswerId === answerId){
+            if(!results[question.id]){
+                results[question.id] = 'success'
+            }
 
             this.setState({
-                answerState: {[answerId]: 'success'}
+                answerState: {[answerId]: 'success'},
+                results
             })
             const timeout= window.setTimeout(()=>{
                if(this.isQuizFinished()){
-                  console.log('Finished')
+                  this.setState({
+                      isFinished:true
+                  })
                }else {
                    this.setState({
                        activeQuestion: this.state.activeQuestion + 1,
@@ -56,17 +72,26 @@ class Quiz extends Component{
                }
                 window.clearTimeout(timeout)
             },1000)
-
-
         } else {
+            results[question.id] = 'error'
             this.setState({
-                answerState:{[answerId]:'error'}
+                answerState:{[answerId]:'error'},
+                results
             })
         }
     }
 
     isQuizFinished(){
     return this.state.activeQuestion +1 === this.state.quiz.length
+    }
+
+    retryHandler=() => {
+        this.setState({
+            activeQuestion:0,
+            answerState: null,
+            isFinished:false,
+            results: {}
+        })
     }
 
 
@@ -76,14 +101,23 @@ class Quiz extends Component{
 
                 <div className={classes.QuizWrapper}>
                     <h1>Answer the questions,please!</h1>
-                    <ActiveQuiz
-                        answers={this.state.quiz[this.state.activeQuestion].answers}
-                        question={this.state.quiz[this.state.activeQuestion].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                        quizLength={this.state.quiz.length}
-                        answerNumber={this.state.activeQuestion + 1}
-                        state={this.state.answerState}
-                        />
+                    {
+                        this.state.isFinished
+                        ? <FinishedQuiz
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+
+                            />
+                            : <ActiveQuiz
+                            answers={this.state.quiz[this.state.activeQuestion].answers}
+                            question={this.state.quiz[this.state.activeQuestion].question}
+                            onAnswerClick={this.onAnswerClickHandler}
+                            quizLength={this.state.quiz.length}
+                            answerNumber={this.state.activeQuestion + 1}
+                            state={this.state.answerState}
+                            />
+                    }
                 </div>
             </div>
         )
